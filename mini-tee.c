24dbc2165dcaf4bf6e1b0aa8f6af16f60a97ecd5
thiagomucci-sgbd <thmucc@protonmail.com>
2026-02-20 21:14:29 -0300

    implemented first while and error handlers

--- mini-tee.c
+++ mini-tee.c
@@ -2,7 +2,42 @@
 #include<unistd.h>
 #include<fcntl.h>
 #include<stdlib.h>
-#define MAX_BUF = 4096
+#define MAX_BUF 4096
 
 int
-main(char *argv[]){}
+main(int argc, char *argv[])
+{
+    int n;
+    char buf[MAX_BUF];
+    if(argc != 2)
+    {
+        write(STDOUT_FILENO,
+            "try: <program> <file>\n",
+                sizeof("try: <program> <file>\n") -1);
+                exit(1);
+    }
+
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if(fd < 0)
+    {
+        perror("open file");
+        exit(1);
+    }
+    
+    while((n = (read(STDIN_FILENO, buf, MAX_BUF)) > 0))
+    {
+        if(write(fd, buf, n) != n)
+        {
+            perror("write");
+            exit(1);
+        }
+    }
+
+    if(n < 0)
+    {
+        perror("read");
+        exit(1);
+    }
+    close(fd);
+
+}
