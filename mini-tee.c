b6cac027898971621f7d071a4a4bd64dee806ade
thiagomucci <thmucc@protonmail.com>
2026-03-06 20:25:00 -0300

    append added

--- mini-tee.c
+++ mini-tee.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 #define MAX_BUF 4096
 
 int
@@ -9,21 +10,32 @@ main(int argc, char *argv[])
 {
     int n;
     char buf[MAX_BUF];
-    if(argc != 2)
+    if(argc != 3)
     {
         write(STDOUT_FILENO,
-            "try: <program> <file>\n",
+            "try: ./prog [-a] <file>\n",
                 sizeof("try: <program> <file>\n") -1);
                 exit(1);
     }
 
-    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	int flags =  O_WRONLY | O_CREAT;
+
+	if((strcmp(argv[1], "-a") == 0))
+	{
+	flags |= O_APPEND;
+	}
+	else
+	{
+		flags |= O_TRUNC;
+	}
+
+    int fd = open(argv[2], flags, 0644);
     if(fd < 0)
     {
         perror("open file");
         exit(1);
     }
-    
+
     while((n = read(STDIN_FILENO, buf, MAX_BUF)) > 0)
     {
         if(write(fd, buf, n) != n)
@@ -49,5 +61,5 @@ main(int argc, char *argv[])
         perror("close");
         exit(1);
     }
-
+	return 0;
 }
